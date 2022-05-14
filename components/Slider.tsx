46c20e5b6d222acba58ea61c50bc9a94f3a93ebc
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2022-05-14 17:59:14 +0430

    change code slider ( class  to function )

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -2,24 +2,25 @@ import React, { Component } from 'react';
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
 import Image from 'next/image';
+import type { NextPage } from "next";
 
-export default class Slider extends Component {
-	render() {
-		return (
-			<div className='my-24'>
-                <h2 className='text-center text-xl font-bold'> مشتریان ما </h2>
-                <Carousel>
-                    <div>
-                        <Image src="/brand/apple.jpg" alt="" width={145} height={120} />
-                    </div>
-                    <div>
-                        <Image src="/brand/adidas.jpg" alt="" width={145} height={120} />
-                    </div>
-                    <div>
-                        <Image src="/brand/samsung.jpg" alt="" width={145} height={120} />
-                    </div>
-                </Carousel>
-			</div>
+const Slider : NextPage = () => {
+    return (
+		<div className='my-24'>
+            <h2 className='text-center text-xl font-bold'> مشتریان ما </h2>
+            <Carousel>
+                <div>
+                    <Image src="/brand/apple.jpg" alt="" width={145} height={120} />
+                </div>
+                <div>
+                    <Image src="/brand/adidas.jpg" alt="" width={145} height={120} />
+                </div>
+                <div>
+                    <Image src="/brand/samsung.jpg" alt="" width={145} height={120} />
+                </div>
+            </Carousel>
+		</div>
 		);
 	}
-};
+
+export default Slider
