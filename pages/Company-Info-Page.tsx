2e75214483c516a941b947e6516a8669b2fd89e3
hazhar ghaderi <hazhar24ghaderi@gmail.com>
2022-05-29 23:19:58 +0430

    post a photo

--- pages/Company-Info-Page.tsx
+++ pages/Company-Info-Page.tsx
@@ -1,57 +1,67 @@
 import next from "next";
 import Link from "next/link";
+import Image from "next/image";
 
 
 export default function Company (){
     return(
-        <div className=" mx-auto md:container md:w-2/3 lg:container lg:w-2/3 mb-10">
-            <div className=" bg-gradient-to-r from-red-600 via-blue-600 to-yellow-300 h-1 rounded-lg mt-1 "></div>
-            
-            
-            
-            
-            
-            <div className=" mt-56">
-                <h1 className=" text-center text-xl text-violet-800 "> راه اندازی سریع </h1>
-                <h1 className=" text-center pt-2"> !مشخصات شرکت را وارد کنید </h1>
+        <div>
+            <div className=" absolute mt-96 pt-20 md:ml-20 lg:ml-56  ">
+               <Image src="/assets/images/gear.png" width={160} height={190} />
             </div>
+            <div className=" absolute mt-0 right-56 sm:right-0 sm:mt-0 md:mt-64 md:right-3 lg:right-32 ">
+                <Image src="/assets/images/operators.png" width={170} height={300} />
+            </div>
+            <div className=" mx-auto md:container lg:container lg:w-2/3 xl:container xl:w-2/3 mb-10">
+                <div className=" bg-gradient-to-r from-red-600 via-blue-600 to-yellow-300 h-1 rounded-lg mt-1 "></div>
+            
+            
+            
+            
+            
+                <div className=" mt-36">
+                    <h1 className=" text-center text-xl text-violet-800 "> راه اندازی سریع </h1>
+                    <h1 className=" text-center pt-2"> !مشخصات شرکت را وارد کنید </h1>
+                </div>
 
-            <form className="mt-6 mx-auto sm:p-4 md:w-4/5 lg:w-2/3 lg:p-3 lg:mx-auto">
-                <div className="flex">
-                    <select className="border-b-4 border-violet-400 hover:border-violet-800 w-56 md:w-56 lg:w-72 mx-auto focus:outline-none text-slate-500">
-                        <option className=" text-right"> نوع شرکت </option>
-                        <option>..........</option>
-                        <option>...............</option>
-                    </select>
-                    <input type="text" placeholder=" نام شرکت " className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-72 lg:pl-3"/>
-                </div>
-                <div className="flex mt-7 ">
-                    <input type="tel" placeholder=" شماره تماس" className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-72 lg:pl-3" />
-                    <input type="tel" placeholder=" فکس "        className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-72 lg:pl-3" />
-                </div>
-                <div className="flex mt-7">
-                    <input type="number" placeholder=" شناسه اقتصادی "  className="text-right border-b-4 pl-2 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-72 lg:pl-3" />
-                    <input type="number" placeholder=" شماره ثبت شرکت " className="text-right border-b-4 pl-10 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-72 lg:pl-3 " />
-                </div>
-                <div>
-                    <p className=" mt-7 text-center"> ( svg | jpg | png فرمت های پشتیبانی شده ) بارگذاری لوگو شرکت </p>
-                </div>
-                <div className="flex mx-auto sm:mx-auto md:mx-auto md:flex mt-7">
-                    
-                    <input type="file" className=" text-sm text-gray-500 file:mr-4 file:py-2 w-96 file:border-0 file:text-sm file:bg-violet-800 file:text-white focus:outline-none pl-3 md:w-96  lg:w-96 lg:pl-3 " />
-                    <div className=" sm:m-0 md:ml-10 lg:ml-14">
-                        <input type="text" placeholder=" هنوز هیچ فایلی انتخاب نشده " className="mx-7 text-right border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none w-56 md:ml-4 md:w-56 lg:w-56" />
+                <form className="mt-4 mx-auto sm:p-4 md:w-4/5 lg:w-3/4 lg:p-3 lg:mx-auto">
+                    <div className="flex">
+                        <select className="border-b-4 border-violet-400 hover:border-violet-800 w-56 md:w-56 lg:w-60 mx-auto focus:outline-none text-slate-500">
+                            <option className=" text-right"> نوع شرکت </option>
+                            <option>..........</option>
+                            <option>...............</option>
+                        </select>
+                        <input type="text" placeholder=" نام شرکت " className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-60"/>
                     </div>
-                </div>
-                <div className=" mt-7">
-                    <input type="text" placeholder=" آدرس " className=" text-right px-4 w-full border-b-4 border-violet-400 hover:border-violet-800 pr-4 pb-10 focus:outline-none" />
-                </div>
-                <div className=" mt-4 text-center pt-3 mx-auto border-2 border-violet-400 text-violet-500 hover:bg-violet-300 hover:text-black w-80 h-12">
-                    <Link href="/"> بعدی </Link>
-                    
-                </div>
-                <p className=" pt-4 text-violet-500 text-center"> بیخیال .... <span className="text-black"> بعدا این هارو پر میکنم. برو به داشبورد </span> </p>
-            </form>
+                    <div className="flex mt-5 ">
+                        <input type="tel" placeholder=" شماره تماس" className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-60 " />
+                        <input type="tel" placeholder=" فکس "        className="text-right pr-4 border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-60 " />
+                    </div>
+                    <div className="flex mt-5">
+                        <input type="number" placeholder=" شناسه اقتصادی "  className="text-right border-b-4 pl-2 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-60 " />
+                        <input type="number" placeholder=" شماره ثبت شرکت " className="text-right border-b-4 pl-10 border-violet-400 hover:border-violet-800 focus:outline-none mx-auto w-56 md:w-56 lg:w-60  " />
+                    </div>
+                    <div>
+                        <p className=" mt-4 text-center"> ( svg | jpg | png فرمت های پشتیبانی شده ) بارگذاری لوگو شرکت </p>
+                    </div>
+                    <div className="flex mx-auto sm:mx-auto md:mx-auto md:flex mt-5">
+                        
+                        <input type="file" className=" text-sm text-gray-500 file:mr-4 file:py-2 w-96 file:border-0 file:text-sm file:bg-violet-800 file:text-white focus:outline-none pl-3 md:w-96  lg:w-96 lg:pl-3 " />
+                        <div className=" sm:m-0 md:ml-10 lg:ml-14">
+                            <input type="text" placeholder=" هنوز هیچ فایلی انتخاب نشده " className="mx-7 text-right border-b-4 border-violet-400 hover:border-violet-800 focus:outline-none w-56 md:ml-4 md:w-56 lg:w-56" />
+                        </div>
+                    </div>
+                    <div className=" mt-4">
+                        <input type="text" placeholder=" آدرس " className=" text-right px-4 w-full border-b-4 border-violet-400 hover:border-violet-800 pr-4 pb-10 focus:outline-none" />
+                    </div>
+                    <div className=" mt-4 text-center pt-3 mx-auto border-2 border-violet-400 text-violet-500 hover:bg-violet-300 hover:text-black w-80 h-12">
+                        <Link href="/"> بعدی </Link>
+                        
+                    </div>
+                    <p className=" pt-4 text-violet-500 text-center"> بیخیال .... <span className="text-black"> بعدا این هارو پر میکنم. برو به داشبورد </span> </p>
+                </form>
+            </div>
         </div>
+       
     )
 }
