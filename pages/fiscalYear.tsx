0c2e9afa86686ba12e5494909fb35495562e021d
amin-hooshmand <aminhoushmand78@gmail.com>
2022-06-01 00:26:14 +0430

    Working On Style

--- pages/fiscalYear.tsx
+++ pages/fiscalYear.tsx
@@ -1,5 +1,6 @@
 import { NextPage } from "next";
 import Image from "next/image";
+import { ChevronDownIcon } from "@heroicons/react/solid";
 import translate from "../public/locales/fa.json";
 
 const FiscalYear: NextPage = () => {
@@ -15,7 +16,6 @@ const FiscalYear: NextPage = () => {
           height={350}
         />
       </div>
-
       <div className="absolute pt-10 mt-52">
         <Image
           src="/assets/images/last.png"
@@ -25,8 +25,34 @@ const FiscalYear: NextPage = () => {
         />
       </div>
       <div className="mx-auto text-center">
-        <div className="mt-60">
-          <h3 className="text-purple-700">{translate.fiscalYear.fiscalYear}</h3>
+        <div className="mt-72">
+          <div className="mb-6">
+            <div className="mb-4">
+              <h4 className="text-2xl text-purple-700 ">
+                {translate.fiscalYear.fiscalYear}
+              </h4>
+            </div>
+            <p>{translate.fiscalYear.descriptionNeeds}</p>
+          </div>
+          <form className="">
+            <div dir="rtl" className="mb-5">
+              <input
+                type="text"
+                className="w-[40%] my-6 text-base border-[3px] border-t-0 border-l-0 border-r-0 focus:outline-none login border-b-fuchsia-500 text-purple-400 "
+                placeholder={translate.fiscalYear.fiscalName}
+              />
+            </div>
+            <div className="relative flex justify-center " dir="rtl">
+              <select className="w-[19%] ml-5  appearance-none  text-base  border-[3px] border-t-0 border-l-0 border-r-0 focus:outline-none login border-b-fuchsia-500 text-purple-400 ">
+                <option>{translate.fiscalYear.dateStart}</option>
+              </select>
+              <ChevronDownIcon className="absolute w-8 h-6 ml-10 text-fuchsia-500" />
+              <select className=" w-[19%]  appearance-none  text-base  border-[3px] border-t-0 border-l-0 border-r-0 focus:outline-none login border-b-fuchsia-500 text-purple-400 ">
+                <option>{translate.fiscalYear.dateEnd}</option>
+              </select>
+              <ChevronDownIcon className="absolute w-8 h-6 left-[470px] text-fuchsia-500" />
+            </div>
+          </form>
         </div>
       </div>
     </div>
